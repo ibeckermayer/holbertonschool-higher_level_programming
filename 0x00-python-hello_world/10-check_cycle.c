c6e88b8c4a1d27c01c381c7a451e5ec790fd30d4
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-04-09 22:12:54 +0000

    updated to meet new reqs

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,7 +1,58 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: original pointer
+ * @old_size: original size of the memory
+ * @new_size: the size of the new memory block
+ *
+ * The contents will be copied to the newly allocated space, in the range
+ * from the start of ptr up to the minimum of the old and new sizes.
+ * If new_size > old_size, the “added” memory should not be initialized.
+ * If new_size == old_size do not do anything and return ptr.
+ * If ptr is NULL, then the call is equivalent to malloc(new_size).
+ * If new_size is equal to zero, and ptr is not NULL, then the call is
+ * equivalent to free(ptr). Return NULL.
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+	char *ptrc;
+	unsigned int iter, i;
+
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		return (p);
+	}
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	ptrc = ptr;
+	iter = old_size > new_size ? new_size : old_size;
+	p = malloc(new_size);
+	if (p == NULL)
+	{
+		/* free(ptr); */
+		return (NULL);
+	}
+
+	for (i = 0; i < iter; i++)
+		p[i] = ptrc[i];
+	free(ptr);
+	return (p);
+}
 
 /**
  * been_seen - checks if a pointer is in the list
@@ -42,7 +93,7 @@ int check_cycle(listint_t *list)
 	{
 		if (num_seen > 1)
 		{
-			seen = realloc(seen, sizeof(*seen) * (num_seen + 1));
+			seen = _realloc(seen, sizeof(*seen) * num_seen, sizeof(*seen) * (num_seen + 1));
 			if (been_seen(seen, list))
 			{
 				free(seen);
