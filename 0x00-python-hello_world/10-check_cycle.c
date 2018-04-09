b9851194a6e2486c29011ef57eb2aea0ef96cdb5
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-04-09 21:15:05 +0000

    adding number 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * been_seen - checks if a pointer is in the list
+ * @seen: list of pointers that have been seen
+ * @list: pointer to be checked
+ *
+ * Return: 1 if list has been seen, 0 otherwise.
+ */
+int been_seen(listint_t **seen, listint_t *list)
+{
+	int i = 0;
+
+	while (seen[i])
+	{
+		if (seen[i] == list)
+			return(1);
+		i++;
+	}
+	return (0);
+}
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: the head of the list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t **seen;
+	int num_seen = 1;
+
+	seen = malloc(sizeof(*seen) * num_seen);
+	seen[0] = list;
+
+	while(list)
+	{
+		if (num_seen > 1)
+		{
+			seen = realloc(seen, sizeof(*seen) * num_seen);
+			if (been_seen(seen, list))
+			{
+				free(seen);
+				return (1);
+			}
+			seen[num_seen - 1] = list;
+		}
+		list = list->next;
+		num_seen++;
+	}
+
+	free(seen);
+	return (0);
+}
