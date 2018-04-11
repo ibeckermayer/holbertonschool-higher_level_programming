ac8de6b828e6916173b4540c7b284295f54b3eb9
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-04-11 05:28:48 +0000

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str=""
+    for c in str:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            new_str+=chr(ord(c) + (ord("A") - ord("a")))
+        else:
+            new_str+=c
+    print(new_str)
