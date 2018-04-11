536c286036d3dcb2a1ff9e22d719a0ca96d2fd62
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-04-11 05:55:33 +0000

    added it to a func

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-for i in range(1, 100):
-    if i % 3 == 0:
-        print("Fizz", end='')
-    if i % 5 == 0:
-        print("Buzz", end='')
-    if i % 3 != 0 and i % 5 != 0:
-        print(i, end='')
-    print(" ", end='')
-print("Buzz ")
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end='')
+        if i % 5 == 0:
+            print("Buzz", end='')
+        if i % 3 != 0 and i % 5 != 0:
+            print(i, end='')
+        print(" ", end='')
+
