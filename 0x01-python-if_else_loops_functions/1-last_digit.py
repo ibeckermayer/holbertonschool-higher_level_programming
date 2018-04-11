c42014d8882f5bc4fbc75a75f317a44071200af8
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-04-11 04:37:56 +0000

    fixed logic

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,18 @@
 import random
 number = random.randint(-10000, 10000)
 outp = "Last digit of {:d} is {:d} "
-if abs(abs(number) % 10) > 5:
-    outp = outp + "and is greater than 5"
-elif abs(abs(number) % 10) == 0:
-    outp = outp + "and is zero"
+if number >= 0:
+	if number % 10 > 5:
+		outp = outp + "and is greater than 5"
+	elif number % 10 == 0:
+		outp = outp + "and is zero"
+	else:
+		outp = outp + "and is less than 6 and not 0"
+	print(outp.format(number, number % 10))
 else:
-    outp = outp + "and is less than 6 and not 0"
-print(outp.format(number, abs(abs(number) % 10)))
+	if number % 10 == 0:
+		outp = outp + "and is zero"
+		print(outp.format(number, number % 10))
+	else:
+		outp = outp + "and is less than 6 and not 0"
+		print(outp.format(number, -(10 - number % 10)))
