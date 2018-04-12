8d268300adf62cafc59ce3bbdb465745a271a08f
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-04-12 19:24:19 +0000

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        if len(sys.argv) == 2:
+            print("1 argument:")
+        else:
+            print("{:d} arguments:".format(len(sys.argv) - 1))
+        c = 1
+        for arg in sys.argv[1:]:
+            print("{:d}: {:s}".format(c, arg))
+            c+=1
