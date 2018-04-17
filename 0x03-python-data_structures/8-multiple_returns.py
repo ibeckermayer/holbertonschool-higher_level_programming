f2aa2272314ec078a1844332061fff3ba54bd9c3
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-04-17 19:50:42 +0000

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        return (0, None)
-    else:
-        return (len(sentence), sentence[0])
+    if sentence is not None:
+        if not sentence:
+            return (0, None)
+        else:
+            return (len(sentence), sentence[0])
