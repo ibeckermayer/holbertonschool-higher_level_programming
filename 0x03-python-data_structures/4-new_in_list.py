c18b4c746e918f374085644baadbc3edd9200b6c
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-04-17 19:11:09 +0000

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    my_list_len = len(my_list)
+    new_list = my_list[:]
+    if idx < 0 or idx > my_list_len - 1:
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
