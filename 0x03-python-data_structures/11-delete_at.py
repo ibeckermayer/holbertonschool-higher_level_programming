04914e6a6b507f21c33ab0d81e3600ed6eb39426
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-04-17 20:04:19 +0000

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    my_list_len = len(my_list)
+    if idx < 0 or idx > my_list_len - 1:
+        return my_list
+    else:
+        del my_list[idx]
+        return my_list
