1e365f4bd3b7a2159a5245179c4fc55d6927ae89
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-04-19 22:16:41 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,8 @@ def roman_to_int(roman_string):
     answer = 0
     if isinstance(roman_string, str):
         roms = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10,
-        'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400,
-        'D': 500, 'CM': 900, 'M': 1000}
+                'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400,
+                'D': 500, 'CM': 900, 'M': 1000}
 
         last = 1
         for i in range(len(roman_string) - 1):
