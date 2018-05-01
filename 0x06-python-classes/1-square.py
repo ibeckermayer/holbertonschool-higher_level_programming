75123f784442c16fbf83c07bc6c7ab410c6a91d5
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-05-01 23:17:34 +0000

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+class Square:
+    """a Square with private size attribute"""
+
+    def __init__(self, size):
+        """The constructor for square
+
+        Args:
+            size (int): Size of the square
+        """
+        self.__size = size
