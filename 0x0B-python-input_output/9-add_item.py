be650587e6dbac8c399ce9360403548a64eb7941
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-05-16 20:34:54 +0000

    9-add_item.py

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+try:
+    args = load_from_json_file(filename)
+except:
+    args = []
+
+args += sys.argv[1:]
+save_to_json_file(args, filename)
