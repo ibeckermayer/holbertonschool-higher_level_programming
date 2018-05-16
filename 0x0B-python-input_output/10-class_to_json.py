7003e6732e2b84de6c6062b9ee262ba712e9a7a5
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-05-16 21:13:17 +0000

    10-class_to_json.py

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+import json
+
+
+def class_to_json(obj):
+    """returns the dictionary description with simple data structure
+       for JSON serialization of an object
+
+    Args:
+       obj (object): a simple data object
+
+    Returns:
+        dict: the dictionary
+
+    """
+    return json.dumps(obj.__dict__)
