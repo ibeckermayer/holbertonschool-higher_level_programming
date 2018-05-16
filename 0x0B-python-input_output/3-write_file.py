d55d3e242f2f9fa2ba3654f3834d68d404cd0d2b
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-05-16 19:37:19 +0000

    3-write_file.py

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8)
+
+    Args:
+       filename (str) (default ""): the file name
+       text     (str) (default ""): the text to be written
+
+    """
+    with open(filename, 'w') as f:
+        return f.write(text)
