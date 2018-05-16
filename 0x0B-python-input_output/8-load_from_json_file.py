b969cab8ebb0b2fcbeccf2762447ff464dbe3491
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-05-16 20:13:42 +0000

    8-load_from_json_file.py

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a 'JSON file'
+
+    Args:
+       filename (str): the file to load from
+
+    Returns:
+        (object): the object loaded
+
+    """
+    with open(filename, 'r', encoding="UTF8") as f:
+        return json.load(f)
