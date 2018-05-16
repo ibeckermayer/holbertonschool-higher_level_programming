8d0dceff4fd99e5c3dca4fc614702b0c8b269e84
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-05-16 19:45:38 +0000

    5-to_json_string.py

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)
+
+    Args:
+       my_obj (object): an object
+
+    Returns:
+        str: an object
+
+    """
+    return json.dumps(my_obj)
