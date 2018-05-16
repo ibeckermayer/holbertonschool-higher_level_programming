d60e374b31939983e12a026f061ed65f42e445a3
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-05-16 19:15:54 +0000

    1-number_of_lines.py

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+def number_of_lines(filename=""):
+    """counts the number of lines in a text file
+
+    Args:
+       filename (str) (default ""): the file name
+
+    Returns:
+        (int): number of lines
+
+    """
+    lines = 0
+    with open(filename, 'r') as f:
+        for l in f:
+            lines += 1
+    return lines
