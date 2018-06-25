5a33291db7b1c41e85ac9956539b2703fda4678b
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-06-25 23:48:54 +0000

    7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
-Enter password: 
-Enter password: 
+-- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+INSERT INTO first_table VALUES (89, "Holberton School");
