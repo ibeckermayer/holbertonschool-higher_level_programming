68591f760834d3d8655d5861c33047f95394c4d7
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-06-26 23:52:59 +0000

    15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
-Enter password: 
-title
-New Girl
-Silicon Valley
-The Big Bang Theory
-The Last Man on Earth
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT tv_shows.title
+FROM tv_genres RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title ASC;
