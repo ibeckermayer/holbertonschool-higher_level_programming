4c9d26154b12a61bc88bc5ab6e2c7a291baf981e
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-06-26 22:30:02 +0000

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,21 +1,4 @@
-Enter password: 
-title   genre_id
-Breaking Bad    1
-Breaking Bad    6
-Breaking Bad    7
-Breaking Bad    8
-Dexter  1
-Dexter  2
-Dexter  6
-Dexter  7
-Dexter  8
-Game of Thrones 1
-Game of Thrones 3
-Game of Thrones 4
-House   1
-House   2
-New Girl    5
-Silicon Valley  5
-The Big Bang Theory 5
-The Last Man on Earth   1
-The Last Man on Earth   5
+-- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
