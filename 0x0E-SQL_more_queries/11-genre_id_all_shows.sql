8af53abc612288370c9656741e6962cad46b9a16
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-06-26 22:37:16 +0000

    11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,23 +1,4 @@
-Enter password: 
-title   genre_id
-Better Call Saul    NULL
-Breaking Bad    1
-Breaking Bad    6
-Breaking Bad    7
-Breaking Bad    8
-Dexter  1
-Dexter  2
-Dexter  6
-Dexter  7
-Dexter  8
-Game of Thrones 1
-Game of Thrones 3
-Game of Thrones 4
-Homeland    NULL
-House   1
-House   2
-New Girl    5
-Silicon Valley  5
-The Big Bang Theory 5
-The Last Man on Earth   1
-The Last Man on Earth   5
+-- lists all shows contained in the database hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
