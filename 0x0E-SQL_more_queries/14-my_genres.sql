291a425a6f8eedde6a61282d3712ca76d08f367e
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-06-26 23:39:34 +0000

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,6 @@
-Enter password: 
-name
-Crime
-Drama
-Mystery
-Suspense
-Thriller
+-- lists all genres of the show Dexter.
+SELECT tv_genres.name
+FROM tv_shows RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
