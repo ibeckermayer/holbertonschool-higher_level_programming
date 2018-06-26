9ce194efc98df706825c912b25ba50f0757fa93a
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-06-26 23:08:53 +0000

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,10 +1,4 @@
-Enter password: 
-genre   number_shows
-Drama   5
-Comedy  4
-Mystery 2
-Crime   2
-Suspense    2
-Thriller    2
-Adventure   1
-Fantasy 1
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
+SELECT tv_genres.name AS genre, COUNT(1) AS number_shows
+FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre ORDER BY number_shows DESC;
