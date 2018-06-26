4ecdd5a0a7abc3b01737202ce3dd27cf34393f08
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-06-26 20:31:32 +0000

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,1 +1,9 @@
-Enter password: 
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS
+states (
+       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE,
+       name VARCHAR(256) NOT NULL
+);
+
+
