74062a4fc1d281c4dd4209547bda3899bd6aafb1
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-06-26 00:24:11 +0000

    15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,2 @@
-Enter password: 
-score   number
-10  2
-8   1
+-- lists the number of records with the same score in the table second_table
+SELECT score, COUNT(1) AS number FROM second_table GROUP BY score ORDER BY score DESC;
