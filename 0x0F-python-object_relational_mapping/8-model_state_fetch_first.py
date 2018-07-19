de09a219ac40bba6f893f9f9cca3dbf6aed416e4
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-07-19 23:38:34 +0000

    8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     from model_state import Base, State
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
-    from sqlalchemy.orm.exc import NoResultFound
 
     mysql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1],
                                                              sys.argv[2],
