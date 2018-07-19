2c3cc5c6e49b720c45b3e8bfba19db8cf2389bda
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-07-19 23:01:14 +0000

    pep8

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     from sqlalchemy.orm import sessionmaker
 
     mysql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1],
-                                                              sys.argv[2],
-                                                              sys.argv[3])
+                                                             sys.argv[2],
+                                                             sys.argv[3])
     engine = create_engine(mysql)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """class definition of a State
 
