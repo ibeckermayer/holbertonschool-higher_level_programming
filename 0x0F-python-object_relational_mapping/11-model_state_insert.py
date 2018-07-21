9426bd6fb53064e11ab961d7e53457da5724ef12
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-07-21 02:58:51 +0000

    11-model_state_insert.py

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+List all State objects with a
+"""
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine, func
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.sql import insert
+
+    mysql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1],
+                                                             sys.argv[2],
+                                                             sys.argv[3])
+    engine = create_engine(mysql)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    conn = engine.connect()
+    ins = insert(State).values({"name": "Louisiana"})
+    conn.execute(ins)
