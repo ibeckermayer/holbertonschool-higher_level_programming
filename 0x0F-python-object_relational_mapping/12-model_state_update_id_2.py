648d6b6afb557e36968efa57e74684304af7aa7b
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-07-21 03:43:13 +0000

    12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+List all State objects with a
+"""
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine, func
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.sql import insert, update
+
+    mysql = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1],
+                                                             sys.argv[2],
+                                                             sys.argv[3])
+    engine = create_engine(mysql)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    u = update(State).where(State.id == 2).values({"name": "New Mexico"})
+    session.execute(u)
