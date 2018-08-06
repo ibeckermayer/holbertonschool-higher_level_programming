7af17482ace003cc899ee5d27b08fb7a770366f2
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-08-06 17:39:46 +0000

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {"email": email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')  # data should be bytes
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_body = response.read()
+        print(the_body)
