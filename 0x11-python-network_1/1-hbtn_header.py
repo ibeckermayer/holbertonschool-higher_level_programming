50beb5935abd098994f23bdfef8f770ef671952b
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-08-06 17:23:24 +0000

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,7 +2,7 @@
 import urllib.request
 import sys
 
-req = urllib.request.Request("https://intranet.hbtn.io/status")
+req = urllib.request.Request(sys.argv[1])
 with urllib.request.urlopen(req) as response:
     the_header = response.info()
     print(the_header["X-Request-Id"])
