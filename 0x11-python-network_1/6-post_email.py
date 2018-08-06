4c2c7dd590540b088e48714233f32045178bdb31
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-08-06 18:17:03 +0000

    pep8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,5 +3,5 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    req = requests.post(sys.argv[1], data = {'email': sys.argv[2]})
+    req = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print(req.text)
