0093e9ac32ca35b41c46811a177c4b5eba336fc9
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-08-06 17:29:49 +0000

    if name == main

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,7 +2,8 @@
 import urllib.request
 import sys
 
-req = urllib.request.Request(sys.argv[1])
-with urllib.request.urlopen(req) as response:
-    the_header = response.info()
-    print(the_header["X-Request-Id"])
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req) as response:
+        the_header = response.info()
+        print(the_header["X-Request-Id"])
