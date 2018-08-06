8b6cff7f6bdcfba69ff5b00efd2f07d524c47205
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-08-06 17:33:03 +0000

    made it technically in line w reqs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
 import urllib.request
 
-req = urllib.request.Request("https://intranet.hbtn.io/status")
-with urllib.request.urlopen(req) as response:
-    the_content = response.read()
-    the_type = type(the_content)
-    the_utf8 = the_content.decode('utf-8')
-    print("Body response:")
-    print("\t- type: {}".format(the_type))
-    print("\t- content: {}".format(the_content))
-    print("\t- utf8 content: {}".format(the_utf8))
+if __name__ == "__main__":
+    req = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        the_content = response.read()
+        the_type = type(the_content)
+        the_utf8 = the_content.decode('utf-8')
+        print("Body response:")
+        print("\t- type: {}".format(the_type))
+        print("\t- content: {}".format(the_content))
+        print("\t- utf8 content: {}".format(the_utf8))
