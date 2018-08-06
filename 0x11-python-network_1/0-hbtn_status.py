9338cb751b7e2cbb9c743903606970112c5d9c6f
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-08-06 17:19:01 +0000

    printing body response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,7 @@ with urllib.request.urlopen(req) as response:
     the_content = response.read()
     the_type = type(the_content)
     the_utf8 = the_content.decode('utf-8')
+    print("Body response:")
     print("\t- type: {}".format(the_type))
     print("\t- content: {}".format(the_content))
     print("\t- utf8 content: {}".format(the_utf8))
