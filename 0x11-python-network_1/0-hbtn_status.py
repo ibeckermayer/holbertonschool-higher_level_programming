c98fa5ff6d721369f430a2f3766031e301e766bd
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-08-06 17:17:46 +0000

    made tab instead of hardcoding spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,11 @@
 
 import urllib.request
 
-req = urllib.request.Request('https://intranet.hbtn.io/status')
-output = '''    - type: {}
-    - content: {}
-    - utf8 content: {}'''
+req = urllib.request.Request("https://intranet.hbtn.io/status")
 with urllib.request.urlopen(req) as response:
     the_content = response.read()
     the_type = type(the_content)
     the_utf8 = the_content.decode('utf-8')
-    print(output.format(the_type, the_content, the_utf8))
+    print("\t- type: {}".format(the_type))
+    print("\t- content: {}".format(the_content))
+    print("\t- utf8 content: {}".format(the_utf8))
