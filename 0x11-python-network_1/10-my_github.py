71296951c4f93399fa341d4487877c04f9712bb0
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-08-06 20:39:52 +0000

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import requests
+from requests.auth import HTTPBasicAuth
 from sys import argv
 
 if __name__ == "__main__":
     url = "https://api.github.com/users"
-    data = {"username": argv[0], "password": argv[1]}
-    req = requests.get(url, data=data)
-    json_dict = req.json()[0]
+    req = requests.get(url, auth=HTTPBasicAuth(argv[1], argv[2]))
+    json_dict = req.json()
     print(json_dict.get("id"))
 
     # try:
