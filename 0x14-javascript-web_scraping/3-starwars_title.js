60fe21a199e172c5065ded9222f4416618b92b86
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-09-06 00:33:12 +0000

    semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,9 +4,9 @@ const request = require('request');
 
 let url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, function (err, resp, body) {
-    if (err) {
-	console.log(err);
-    } else {
-	console.log(JSON.parse(body).title);
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
