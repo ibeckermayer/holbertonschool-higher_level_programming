020395d8976d7fa52ece74d791cfd5039677554a
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-09-06 00:25:15 +0000

    semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], function (err) {
-    if (err) {
-	console.log(err);
-    }
+  if (err) {
+    console.log(err);
+  }
 });
