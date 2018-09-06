0cdf04760989aca4a773c81a1ed62a2577815f7b
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2018-09-06 00:20:57 +0000

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const file = process.argv[2];
 
 fs.readFile(file, function (err, data) {
-    if (err) {
-	console.log(err);
-    } else {
-	console.log(data.toString());
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
 });
